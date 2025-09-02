909fe065a170a67ffa42d4095e743118761d4132
AnilKumarVijayagiri <121851145+AnilKumarVijayagiri@users.noreply.github.com>
2025-09-03 00:47:40 +0530

    Update server.js

--- src/server.js
+++ src/server.js
@@ -26,7 +26,7 @@ const app = express();
 app.use(express.json({ limit: '2mb' }));
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({
-  origin: process.env.CLIENT_URL || "https://shaivyah-client.vercel.app", // your frontend URL
+  origin: ["http://localhost:5173", "https://shaivyah-client.vercel.app"], // your frontend URL
   credentials: true, // allow cookies/auth headers
 }));
 app.use(morgan('dev'));
